d53521d6ed7e9c924d68937c9a727b50fe197cc5
vladlen584 <63865315+vladlen584@users.noreply.github.com>
2024-03-31 23:32:02 +0300

    Update CPP.cpp

--- CPP.cpp
+++ CPP.cpp
@@ -717,6 +717,41 @@ void Lab7()
     cout << "В разработке!";
 }
 
+void Lab11()
+{
+
+    int n=1;
+    std::cout << "Введите размер массива: ";
+    std::cin >> n;
+
+    int* arr = new int[n];
+    std::cout << "Введите " << n << " целых чисел: ";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    std::cout << "Отсортированный массив: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Обмен значениями
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
 
 
 
